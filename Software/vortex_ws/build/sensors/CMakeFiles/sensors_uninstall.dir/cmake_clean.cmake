46e030db5dc457cd9879638532093e529a7accf7
Your Name <you@example.com>
2020-12-25 20:06:42 +0200

    ros packages initialization

--- Software/vortex_ws/build/sensors/CMakeFiles/sensors_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sensors_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sensors_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
